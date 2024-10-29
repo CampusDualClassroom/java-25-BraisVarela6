95c785ce303bb20e30667bb9b57085f2b709a650
BraisVarela <braisvarela6@gmail.com>
2024-10-29 16:05:10 +0100

    Ejercicio 25 Hecho

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,28 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
+        Stack<String> pila = new Stack<>();
 
+        pila.push("Smith");
+        pila.push("Montessori");
+        pila.push("Peralta");
+        pila.push("House");
+
+        //System.out.println(pila);
+
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        for (int i = stack.size(); i >= 1; i--) {
+            //System.out.println(stack.peek());
+            System.out.println(stack.peek());
+            stack.pop();
+        }
     }
 
     public static void main(String[] args) {
-
+        printAndEmptyStack(createStack());
     }
 
 
